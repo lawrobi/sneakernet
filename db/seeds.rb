08dd44e5dd1878d80e13b4077f244d7d9fcc1eed
Gabe Kopley <gabe@coshx.com>
2013-03-02 21:09:00 -0800

    City seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seed US cities to places table
+require 'csv'
+file = File.join(Rails.root, 'config', 'data', 'us_cities_by_population.csv')
+cities = CSV.read(file)
+cities.each do |city|
+  place = Place.find_or_initialize_by_city_and_state(city[0], city[1])
+  place.country = "United States"
+  place.display_name = "#{place.city}, #{place.state}"
+  place.save
+end
+
+# world cities
+file = File.join(Rails.root, 'config', 'data', 'world_cities.csv')
+cities = CSV.read(file)
+cities.reject {|c| c[1] == "United States"}.each do |city|
+  place = Place.find_or_initialize_by_city_and_country(city[2], city[1])
+  place.display_name = "#{place.city}, #{place.country}"
+  place.save
+end

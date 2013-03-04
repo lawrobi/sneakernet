4f9d2ef9726715ac172cf4145327300cbb9151f1
Gabe Kopley <gabe@coshx.com>
2013-03-03 19:36:02 -0800

    Fix LIKE match

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -7,7 +7,7 @@ class Api::PlacesController < ApplicationController
   def index
     @places = Place.scoped
     if params[:q]
-      places = @places.where("display_name like ?", "%#{params[:q]}%")                                .order("population desc").limit(50)
+      places = @places.where("display_name like ?", "#{params[:q]}%")                                .order("population desc").limit(50)
       us_places = places.select {|p| p.country == "United States"}
       world_places = places - us_places
       @places = (us_places + world_places).take(10)
